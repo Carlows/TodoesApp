eb146f1228780b06698add5739d762999cf9c7d2
Carlos Martinez <carlosjesus94@gmail.com>
2015-03-05 11:23:41 -0430

    added webapi and mvc, fixed controllers

--- NotasApp.Site/Controllers/TodosController.cs
+++ NotasApp.Site/Controllers/TodosController.cs
@@ -12,6 +12,7 @@ using NotasApp.Site.Models;
 
 namespace NotasApp.Site.Controllers
 {
+    [ExceptionAttribute]
     [RoutePrefix("api/Todos")]
     public class TodosController : ApiController
     {
@@ -22,10 +23,60 @@ namespace NotasApp.Site.Controllers
             _todoItems = todoitemsRepo;
         }
         
-        [ExceptionAttribute]
-        public IEnumerable<TodoItemModel> Get()
+        public IHttpActionResult Get()
         {
-            return Mapper.Map<List<ToDoItem>, List<TodoItemModel>>(_todoItems.GetToDoItems().ToList());
+            var todoes = Mapper.Map<List<ToDoItem>, List<TodoItemModel>>(_todoItems.GetToDoItems().ToList());
+
+            return Ok(todoes);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var todo = _todoItems.GetById(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<ToDoItem, TodoItemModel>(todo));
+        }
+
+        public IHttpActionResult Post(TodoItemModel todo)
+        {
+            var model = Mapper.Map<TodoItemModel, ToDoItem>(todo);
+            model.FechaCreacion = DateTime.Now;
+            model = _todoItems.Add(model);
+            todo = Mapper.Map<ToDoItem, TodoItemModel>(model);
+
+            return Created(Url.Link("DefaultApi", new {controller = "Todos", id = todo.ToDoItemID}), todo);
+        }
+
+        public IHttpActionResult Put(int id, TodoItemModel todo)
+        {
+            todo.ToDoItemID = id;
+            todo.FechaCreacion = DateTime.Now;
+
+            var todoModel = _todoItems.GetById(id);
+
+            if (todoModel == null)
+            {
+                return NotFound();
+            }
+
+            Mapper.Map(todo, todoModel);
+
+            todoModel = _todoItems.Update(todoModel);
+            todo = Mapper.Map<ToDoItem, TodoItemModel>(todoModel);
+
+            return Ok(todo);
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _todoItems.Delete(id);
+
+            return Ok();
         }
     }
 }
